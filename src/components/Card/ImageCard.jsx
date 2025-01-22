04eb783ce0a7e5b8e8424d61d55553246bea5c47
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-01-22 15:11:39 +0100

    remove some style from index.css and edit sidebar and imageCard

--- src/components/Card/ImageCard.jsx
+++ src/components/Card/ImageCard.jsx
@@ -2,10 +2,10 @@ import PropTypes from "prop-types"
 
 const ImageCard = ({ children, imgSrc, ...rest }) => {
     return (
-        <div {...rest} className="relative max-w-sm rounded-md overflow-hidden group max-h-[36rem]">
+        <div {...rest} className="relative max-w-sm rounded-md overflow-hidden max-h-[36rem]">
             <img loading="lazy" src={imgSrc} alt="" className="group-hover:scale-150 transition-transform duration-500" />
-            <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/80 to-transparent">
-                <div className="p-4 text-white space-y-3 relative top-full group-hover:top-0 transition-[top] duration-500">
+            <div className=" group absolute inset-0 flex items-end bg-gradient-to-t from-black/80 to-transparent">
+                <div className="p-4 text-white space-y-3 relative top-full group-hover:top-0 transition-[top,position] duration-500">
                     {children}
                 </div>
             </div>
--- src/components/SideBar2/SideBar.jsx
+++ src/components/SideBar2/SideBar.jsx
@@ -31,16 +31,13 @@ const SideBar = () => {
                     {menus.map((menu, i) => (
                         <li key={i}
                             onClick={() => selectMenu(menu.title)}
-                            className={`text-gray-300 transition-all duration-500 text-sm flex items-center gap-x-4 cursor-pointer p-2 rounded-md side-item ${menu.gap ? "mt-9" : "mt-2"} ${menu.selected ? "bg-zinc-500/50" : "hover:bg-light-white"}`}
+                            className={`group text-gray-300 transition-all duration-500 text-sm flex items-center gap-x-4 cursor-pointer p-2 rounded-md side-item ${menu.gap ? "mt-9" : "mt-2"} ${menu.selected ? "bg-zinc-500/50" : "hover:bg-light-white"}`}
                         >
                             <menu.Icon className="min-h-6 min-w-6" />
                             <span className={`text-white text-nowrap origin-left duration-500 ${open ? "scale-100" : "scale-0 delay-200"}`}>{menu.title}</span>
-                            <span className={`${!open && "tooltip"} invisible absolute left-16 p-2 rounded-md bg-dark-purple/30 text-dark-purple transition-colors`}>{menu.title}</span>
+                            <span className={`${open ? "invisible" : "group-hover:opacity-100 group-hover:visible"} invisible opacity-0 absolute left-16 p-2 rounded-md bg-dark-purple/30 text-dark-purple transition-[background,opacity] duration-300`}>{menu.title}</span>
                         </li>
                     ))}
-                    <div className="absolute h-9 bg-zinc-50 rounded-lg top-2 left-2 right-2 -z-10 transition-[top,left] duration-500">
-
-                    </div>
                 </ul>
             </div>
             <div className="p-7 text-2xl font-semibold flex-1 h-screen">
